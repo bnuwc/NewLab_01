e491247bfb2023b42d302248f655eb3fd29c2527
LAB211-08\CAMT <kitsada_tang@cmu.ac.th>
2017-03-13 17:37:23 +0700

    A Push Method1

--- src/main/java/utility/Currency.java
+++ src/main/java/utility/Currency.java
@@ -4,7 +4,55 @@ package utility;
  * Created by CAMT on 3/13/2017.
  */
 public class Currency {
-    public double exchange(String sourceCurrency,double amount,String targetCurrency){
-        return 0.0;
+    String[] currency = {"USD","EUR","THB"};
+
+    public double exchange (String sourceCurrency, double amount, String targetCurrency){
+
+        OUTER:
+        {
+            for (int i = 0; i < currency.length; i++) {
+                if (sourceCurrency == currency[i]) break OUTER;
+            }
+            return Double.NaN;
+        }
+
+        OUTER:
+        {
+            for (int i = 0; i < currency.length; i++) {
+                if (targetCurrency == currency[i]) break OUTER;
+            }
+            return Double.NaN;
+        }
+
+        if(sourceCurrency == targetCurrency){
+            return amount;
+        }else if (sourceCurrency == "USD"){
+            return sourceIsUSD(sourceCurrency,amount,targetCurrency);
+        }else if (targetCurrency == "USD"){
+            return targetIsUSD(sourceCurrency,amount,targetCurrency);
+        }else{
+            return bothIsNotUSD(sourceCurrency,amount,targetCurrency);
+        }
+    }
+    private double targetIsUSD (String sourceCurrency, double amount, String targetCurrency){
+        if(sourceCurrency=="THB"){
+            amount = amount/33.0;
+            return amount;
+        }else{
+            amount = amount / 1.01;
+            return amount;
+        }
+    }
+
+    private double bothIsNotUSD (String sourceCurrency, double amount, String targetCurrency) {
+        if (sourceCurrency == "THB") {
+            amount = amount / 33.0;
+            amount = amount * 1.01;
+            return amount;
+        }else {
+            amount = amount / 1.01;
+            amount = amount * 33;
+            return amount;
+        }
     }
 }
