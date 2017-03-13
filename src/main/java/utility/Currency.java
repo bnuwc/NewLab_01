2945e2aca5d2f7e04b8b22064f0c8db7eb15a1d0
thapakorn <thapakorn_t@cmu.ac.th>
2017-03-13 17:36:27 +0700

    source currency is USD

--- src/main/java/utility/Currency.java
+++ src/main/java/utility/Currency.java
@@ -4,7 +4,45 @@ package utility;
  * Created by CAMT on 3/13/2017.
  */
 public class Currency {
-    public double exchange(String sourceCurrency,double amount,String targetCurrency){
-        return 0.0;
+    String[] currency = {"USD","EUR","THB"};
+
+    public double exchange (String sourceCurrency, double amount, String targetCurrency){
+
+        OUTER:
+        {
+            for (int i = 0; i < currency.length; i++) {
+                if (sourceCurrency == currency[i]) break OUTER;
+            }
+            return Double.NaN;
+        }
+
+        OUTER:
+        {
+            for (int i = 0; i < currency.length; i++) {
+                if (targetCurrency == currency[i]) break OUTER;
+            }
+            return Double.NaN;
+        }
+
+        if(sourceCurrency == targetCurrency){
+            return amount;
+        }else if (sourceCurrency == "USD"){
+            return sourceIsUSD(sourceCurrency,amount,targetCurrency);
+        }else if (targetCurrency == "USD"){
+            return targetIsUSD(sourceCurrency,amount,targetCurrency);
+        }else{
+            return bothIsNotUSD(sourceCurrency,amount,targetCurrency);
+        }
+    }
+
+
+    private double sourceIsUSD (String sourceCurrency, double amount, String targetCurrency){
+        if(targetCurrency=="THB"){
+            amount = amount * 33.0;
+            return amount;
+        }else{
+            amount = amount * 1.01;
+            return amount;
+        }
     }
 }
