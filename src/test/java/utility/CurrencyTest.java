e7a40936d05aaf8f9a83eb8be54c6e5a84df5c48
thapakorn <thapakorn_t@cmu.ac.th>
2017-03-13 17:51:18 +0700

    method

--- src/test/java/utility/CurrencyTest.java
+++ src/test/java/utility/CurrencyTest.java
@@ -15,7 +15,7 @@ public class CurrencyTest {
         Currency currency = new Currency();
         assertThat(currency.exchange("USD",1.0,"USD"),closeTo(1.0,0.001));
 
- assertThat(currency.exchange("USD",2.0,"USD"),closeTo(2.0,0.001));
+        assertThat(currency.exchange("USD",2.0,"USD"),closeTo(2.0,0.001));
 
         assertThat(currency.exchange("THB",5.0,"EUR"),closeTo(3.5,0.001));
 
